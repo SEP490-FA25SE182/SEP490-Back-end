0d86be138430b7f1e7f1879f3d377c592bbfd8f5
tuyenctnse182129 <chauthingoctuyen24@gmail.com>
2025-10-27 14:42:29 +0700

    fix: default for user address

--- rookie-service/src/main/java/com/sep/rookieservice/repository/UserAddressRepository.java
+++ rookie-service/src/main/java/com/sep/rookieservice/repository/UserAddressRepository.java
@@ -1,12 +1,19 @@
 package com.sep.rookieservice.repository;
 
 import com.sep.rookieservice.entity.UserAddress;
+import com.sep.rookieservice.enums.IsActived;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserAddressRepository extends JpaRepository<UserAddress, String> {
     List<UserAddress> findByUserId(String userId);
+    List<UserAddress> findByUserIdAndIsActived(String userId, IsActived isActived);
+
+    long countByUserIdAndIsDefaultTrueAndIsActived(String userId, IsActived isActived);
+    List<UserAddress> findAllByUserIdAndIsDefaultTrueAndIsActived(String userId, IsActived isActived);
+    Optional<UserAddress> findFirstByUserIdAndIsDefaultTrueAndIsActived(String userId, IsActived isActived);
 }
