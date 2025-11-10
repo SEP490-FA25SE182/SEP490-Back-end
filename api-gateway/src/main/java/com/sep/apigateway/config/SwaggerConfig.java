1bc3e919a82e9ca7f9c280c15dfcd5e8bfdcd943
VuQuangMinh1212 <vuquangminh12122004@gmail.com>
2025-11-10 13:50:00 +0700

    Update SwaggerConfig.java

--- api-gateway/src/main/java/com/sep/apigateway/config/SwaggerConfig.java
+++ api-gateway/src/main/java/com/sep/apigateway/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package com.sep.apigateway.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,6 +19,11 @@ public class SwaggerConfig {
                         .title("AI-Powered AR Book API")
                         .version("1.0.0")
                         .description("API documentation for AR Book microservice")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .servers(Arrays.asList(
+                        new Server().url("/api").description("API Gateway"),
+                        new Server().url("/rookie-service").description("Rookie Service"),
+                        new Server().url("/ar-service").description("AR Service")
+                ));
     }
 }
