53adfd13b0cec9336faa2622ce12812b493d2b6d
hoangmcman <133946135+tuyenctnse182129@users.noreply.github.com>
2025-12-05 15:54:44 +0700

    fix: cors m

--- api-gateway/src/main/java/com/sep/apigateway/config/CorsGlobalConfig.java
+++ api-gateway/src/main/java/com/sep/apigateway/config/CorsGlobalConfig.java
@@ -1,4 +1,4 @@
-package com.sep.rookieservice.config;
+package com.sep.apigateway.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
